c528bbdbac8c60043f20f18aa211a4d737a93a8f
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-07-20 11:38:31 +0700

    Frontend

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let myFunction = function(){
     setInterval(function () {
         $.ajax({
             type: "GET",
-            url: "http://ecourse.cpe.ku.th:1515",
+            url: "http://ecourse.cpe.ku.th:1515/api/buapalm-light_lux/view/",
             data: "text",
             success: function (response) {
-
+                
             },
             fail: function(response){
                 console.log(response)
@@ -13,7 +13,24 @@ let myFunction = function(){
         });
     }, 5000)
 
-    
+    $("#light").on("click", function () {
+        $.ajax({
+            type: "POST",
+            url: "http://ecourse.cpe.ku.th:1515/buapalm-light_lux/api/set/",
+            data: {
+                value: $(response).val()
+            },
+            dataType: "json",
+            success: function (response) {
+                console.log(response)
+                if(response === 0){
+                    response = 1;
+                }else{
+                    response = 0;
+                }
+            }
+        });
+    });
 
 }
 
